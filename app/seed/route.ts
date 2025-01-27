25cc64a5fa975cf0d77759af01977a1135644a6f
CharlesBZ <charles.a.bestjr@gmail.com>
2025-01-27 00:24:42 -0500

    data seeded succesfully

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+/* import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
@@ -102,5 +102,22 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return;
+  /* return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  }); */
+  try {
+    await client.sql`BEGIN`;
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
+    await client.sql`COMMIT`;
+
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    await client.sql`ROLLBACK`;
+    return Response.json({ error }, { status: 500 });
+  }
 }
+*/
